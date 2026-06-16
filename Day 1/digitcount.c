5318a75dc261894b371ceb95766819bf87802574
Asmit-010 <25116cn238@glbitm.ac.in>
2026-06-16 12:42:17 +0530

    DAY 1

---
+++ Day 1/digitcount.c
@@ -0,0 +1,15 @@
+//count digits in a number 
+#include<stdio.h>
+int main()
+
+{int n,count=0;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        n=n/10;
+        count++;
+    }
+    printf("number of digits: %d",count);
+    return 0;
+}
---
+++ Day 1/multiplication.c
@@ -0,0 +1,13 @@
+//multiplication table of a given number 
+#include<stdio.h>
+int main (){
+    int a,b,product;
+    printf("enter first number:");
+    scanf("%d",&a);
+    printf("enter second number: ");
+    scanf("%d",&b);
+    product= a * b;
+    printf("the product of %d and %d is %d /n", a,b,product);
+    
+    return 0;
+}
